e7818afc67d833e2eca26baac59975d6d6f43637
Marco Vinci <m.vinci16@studenti.uniba.it>
2026-03-20 17:13:33 +0100

    feat: :sparkles: Metriche per la valutazione

---
+++ src/computer_vision/metrics.py
@@ -0,0 +1,28 @@
+from sktime.performance_metrics.forecasting import (
+    MeanAbsoluteError,
+    MeanAbsolutePercentageError,
+    MeanSquaredError,
+)
+
+
+class RootMeanSquaredError(MeanSquaredError):
+    def __init__(
+        self,
+        multioutput="uniform_average",
+        multilevel="uniform_average",
+        by_index=False,
+    ):
+        super().__init__(
+            multioutput=multioutput,
+            multilevel=multilevel,
+            by_index=by_index,
+            square_root=True,
+        )
+
+
+metrics = {
+    "MAE": MeanAbsoluteError(),
+    "MAPE": MeanAbsolutePercentageError(),
+    "RMSE": RootMeanSquaredError(),
+    "MSE": MeanSquaredError(),
+}
