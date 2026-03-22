06a0dd239cd49e43b85398808012b7430c985e3a
Marco Vinci <m.vinci16@studenti.uniba.it>
2026-03-22 09:09:59 +0100

    feat: :sparkles: Bozza di LSTMForecaster

--- src/computer_vision/forecasters.py
+++ src/computer_vision/forecasters.py
@@ -7,6 +7,7 @@ if TYPE_CHECKING:
 from sktime.forecasting.compose import ForecastingPipeline
 from sktime.forecasting.darts import DartsXGBModel
 from sktime.forecasting.naive import NaiveForecaster
+from sktime.forecasting.neuralforecast import NeuralForecastLSTM
 from sktime.transformations.compose import ColumnEnsembleTransformer, Id
 from sktime.transformations.series.date import DateTimeFeatures
 from sktime.transformations.series.impute import Imputer
@@ -24,6 +25,9 @@ def create_forecaster(forecaster_name: str, params: dict) -> ForecastingPipeline
 
     if forecaster_name == "darts_xgb":
         return create_darts_xgb_forecaster(**params)
+
+    if forecaster_name == "lstm":
+        return create_lstm_forecaster(**params)
 
     raise ValueError(f"Unknown forecaster: {forecaster_name}")
 
@@ -109,6 +113,65 @@ def create_darts_xgb_forecaster(
         lags=lags,
         output_chunk_length=output_chunk_length,
         random_state=random_state,
+    )
+    pipeline = X_transformers ** (y_transformers * forecaster)
+    return pipeline
+
+
+def create_lstm_forecaster(
+    lags: int,
+    output_chunk_length: int,
+    random_seed: int,
+    loss: "str | keras.Metric",
+    metrics: "collections.abc.Sequence[str | keras.Metric] | None",
+    optimizer: "str | keras.optimizers.Optimizer",
+) -> ForecastingPipeline:
+
+    import torch.nn as nn
+    import torch.optim
+
+    _dtfeats_transformer = DateTimeFeatures() * ColumnEnsembleTransformer(
+        [
+            ("id", Id(), ["year"]),
+            (
+                "cyclical",
+                CyclicalEncodingTransformer(),
+                ["month_of_year", "day_of_week", "hour_of_day"],
+            ),
+        ],
+        feature_names_out="original",
+    )
+
+    X_transformers = _dtfeats_transformer
+
+    y_transformers = Imputer()
+
+    losses = {
+        "mse": nn.MSELoss(),
+        "huber": nn.HuberLoss(),
+    }
+
+    optimizers = {
+        "adam": torch.optim.Adam,
+        "sgd": torch.optim.SGD,
+    }
+
+    forecaster = NeuralForecastLSTM(
+        freq="15min",
+        futr_exog_list=[
+            "year",
+            "month_of_year_sin",
+            "month_of_year_cos",
+            "day_of_week_sin",
+            "day_of_week_cos",
+            "hour_of_day_sin",
+            "hour_of_day_cos",
+        ],
+        verbose_fit=True,
+        verbose_predict=True,
+        loss=losses[loss] if isinstance(loss, str) else loss,
+        optimizer=optimizers[optimizer] if isinstance(optimizer, str) else optimizer,
+        random_seed=random_seed,
     )
     pipeline = X_transformers ** (y_transformers * forecaster)
     return pipeline
