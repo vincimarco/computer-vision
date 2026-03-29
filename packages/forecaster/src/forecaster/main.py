76ee3558d98058b1dc9c87aa2f1bf52ebbd329ae
Marco Vinci <m.vinci16@studenti.uniba.it>
2026-03-29 18:37:03 +0200

    fix: :bug: Nome file forecaster

--- packages/forecaster/src/forecaster/main.py
+++ packages/forecaster/src/forecaster/main.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 
 from loguru import logger
 from sktime.forecasting.compose import ForecastingPipeline
@@ -6,28 +7,21 @@ from sktime.forecasting.compose import ForecastingPipeline
 
 class Config:
     def __init__(self):
-        self.model_file = os.environ["FORECASTER_MODEL_FILE"]
-
-        self.mqtt_host = os.environ["METER_MQTT_HOST"]
-        self.mqtt_port = int(os.environ["METER_MQTT_PORT"])
-        self.mqtt_username = os.environ["METER_MQTT_USERNAME"]
-        self.mqtt_password = os.environ["METER_MQTT_PASSWORD"]
-
-        self.ca_cert = os.environ.get("METER_CA_CERT")
-        self.certfile = os.environ.get("METER_CERTFILE")
-        self.keyfile = os.environ.get("METER_KEYFILE")
+        self.model_name = os.getenv("FORECASTER_MODEL_NAME", "model")
 
 
 def main() -> None:
     config = Config()
 
-    forecaster = load_forecaster(model_file=config.model_file)
-    print(forecaster.cutoff\)
+    forecaster = load_forecaster(model_name=config.model_name)
+    print(forecaster.cutoff)
 
 
-def load_forecaster(model_file: str | None = None) -> ForecastingPipeline:
+def load_forecaster(model_name: str | None = None) -> ForecastingPipeline:
     logger.info("Loading forecaster...")
-    forecaster: ForecastingPipeline = ForecastingPipeline.load_from_path(model_file)
+    forecaster: ForecastingPipeline = ForecastingPipeline.load_from_path(
+        pathlib.Path(f"/forecaster/{model_name}.zip")
+    )
     logger.info("Forecaster loaded successfully!")
     logger.debug(f"Forecaster details: {forecaster}")
     return forecaster
