c8e2a893270304142c3c48419e720a6fc4262a7b
Marco Vinci <m.vinci16@studenti.uniba.it>
2026-04-06 11:07:21 +0200

    fix: :bug: Riaggiustato export

--- src/computer_vision/export.py
+++ src/computer_vision/export.py
@@ -1,15 +1,23 @@
 import datetime
 
+import pytz
+
 from .config import EXPORT_DATA_DIR, params
 from .dataset import load_dataset
 
 
 def export():
-    X, y = load_dataset(params["general"]["customer_ids"])
+    X, y = load_dataset(
+        params["general"]["customer_ids"],
+    )
 
     for id in params["general"]["customer_ids"]:
         group = y.xs(id)
-        group.index = group.index + datetime.timedelta(669)
+        group = group[
+            group.index
+            >= datetime.datetime(2020, 9, 1, tzinfo=pytz.timezone("America/Montevideo"))
+        ]
+        group = group.dropna()
         group = group.sort_index()
         group.to_parquet(f"{EXPORT_DATA_DIR}/{id}.parquet")
     # Export logic here
