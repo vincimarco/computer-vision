b4e718a304f89988737c5fdc90c5a0785ba2ede7
Marco Vinci <m.vinci16@studenti.uniba.it>
2026-04-06 15:10:38 +0200

    fix: :bug: Inizio e fine export

--- src/computer_vision/export.py
+++ src/computer_vision/export.py
@@ -1,22 +1,27 @@
-import datetime
-
-import pytz
+import pandas as pd
 
 from .config import EXPORT_DATA_DIR, params
 from .dataset import load_dataset
 
 
 def export():
+    start_date = params["general"]["cutoff"]
+    start_date = pd.to_datetime(start_date, format="ISO8601", utc=True).tz_convert(
+        "America/Montevideo"
+    )
+    end_date = params["test"]["end_date"]
+    end_date = pd.to_datetime(end_date, format="ISO8601", utc=True).tz_convert(
+        "America/Montevideo"
+    )
+
     X, y = load_dataset(
         params["general"]["customer_ids"],
+        start_date=start_date,
+        end_date=end_date,
     )
 
     for id in params["general"]["customer_ids"]:
         group = y.xs(id)
-        group = group[
-            group.index
-            >= datetime.datetime(2020, 9, 1, tzinfo=pytz.timezone("America/Montevideo"))
-        ]
         group = group.dropna()
         group = group.sort_index()
         group.to_parquet(f"{EXPORT_DATA_DIR}/{id}.parquet")
